0abd34046cfeed9768585a809da37261300ee417
DopplerKuo <doppler.kuo@gmail.com>
2018-07-31 23:32:05 +0800

    notthisplz

--- public/assets/js/aboutScroll.js
+++ public/assets/js/aboutScroll.js
@@ -8,7 +8,7 @@ function readDeviceOrientation() {
 
 $ (document).ready (function () {
   // Add smooth scrolling to all links
-  $ ('a').on ('click', function (event) {
+  $ ('a:not(#notthisplz)').on ('click', function (event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== '') {
       // Prevent default anchor click behavior
